750bca548ed4df85604f1701d07a85bcc7d25bf4
Tyler <tyler.viducic@gmail.com>
2023-11-12 13:21:41 -0500

    order of magnitude

--- scripts/season_from_gameid.py
+++ scripts/season_from_gameid.py
@@ -11,6 +11,6 @@ df = pd.read_csv(
     '/Users/tylerviducic/dev/hockey_analytics/gamescore_model/data/kaggle_data/final_scores.csv')
 
 df['season'] = np.where(
-    True, df['game_id']/100000, 0).astype(int)
+    True, df['game_id']/1000000, 0).astype(int)
 
 df.to_csv('/Users/tylerviducic/dev/hockey_analytics/gamescore_model/data/kaggle_data/final_scores.csv', index=False)
