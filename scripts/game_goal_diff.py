30101ba99564a17fba21fd802fcafa9dcccc0138
Tyler <tyler.viducic@gmail.com>
2023-11-19 16:49:50 -0500

    fixed massive typo in calculator. ugh

--- scripts/game_goal_diff.py
+++ scripts/game_goal_diff.py
@@ -6,7 +6,7 @@ df = pd.read_csv(
     '/Users/tylerviducic/dev/hockey_analytics/gamescore_model/data/cleaned_data/whole_season/all_player_games.csv')
 
 df['game_goal_diff'] = np.where(
-    'home_or_away' == 'HOME', df['home_score'] - df['away_score'], df['away_score'] - df['home_score'])
+    df['home_or_away'] == 'HOME', df['home_score'] - df['away_score'], df['away_score'] - df['home_score'])
 
 df.to_csv(
     '/Users/tylerviducic/dev/hockey_analytics/gamescore_model/data/cleaned_data/whole_season/all_player_games.csv')
